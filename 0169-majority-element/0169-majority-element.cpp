e6f76eb816d66ea04e00ac9019160d80a84e0159
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-01-22 01:48:32 +0600

    Time: 28 ms (43.08%), Space: 19.6 MB (23.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,7 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         
          sort(nums.begin(), nums.end());
-        return nums[(nums.size()-1)/2];
+        return nums[(nums.size())/2];
         
     }
 };
