3ab98948bd7d4c29d1367dc741c92ec71c9be3d2
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-01-23 00:10:37 +0600

    Added solution - LeetHub

---
+++ Sum of upper and lower triangles - GFG/sum-of-upper-and-lower-triangles.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to return sum of upper and lower triangles of a matrix.
+    vector<int> sumTriangles(const vector<vector<int> >& matrix, int n)
+    {
+        vector<int>ans;
+        
+        int sum1=0,sum2=0;
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i; j<n; j++)
+            {
+                sum1+=matrix[i][j];
+                sum2+=matrix[j][i];
+            }
+        }
+        
+   ans.push_back(sum1);
+   ans.push_back(sum2);
+        
+        
+        return ans;
+        // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        Solution ob;
+        vector<int> result = ob.sumTriangles(matrix,n);
+        for (int i = 0; i < result.size(); ++i)
+            cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
