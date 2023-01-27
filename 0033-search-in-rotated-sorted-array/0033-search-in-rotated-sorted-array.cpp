f584e3f790b9c4902142e5bf198c7097da4645af
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-01-27 07:17:31 +0600

    Time: 0 ms (100.00%), Space: 11.2 MB (40.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    
+    //normal binary search
     int BS(vector<int>&nums, int s, int e, int t)
     {
-        
         int mid;
         int ans=-1;
         while(s<=e)
@@ -22,18 +21,18 @@ public:
         return ans;
     }
     
+    //main code
     int search(vector<int>& nums, int target) {
         
         
         int s=0;
         int e=nums.size()-1;
         int mid;
-         int ind;
-        if(nums[0]<=nums[e])
+        int ind;  //min value index ind te store korbo
+        if(nums[0]<=nums[e])//jodi kono rotation na thake tahole min value index hobe 0 
         {
              ind = 0;
         }
-        
         while(s<=e)
         {
             mid=s+(e-s)/2;
@@ -46,22 +45,27 @@ public:
                 ind = mid;
                 break;
             }
-            else if(nums[0]<=nums[mid])
+            else if(nums[0]<=nums[mid]) //jodi left part sorted thake
             {
                 s=mid+1;
             }
-            else if(nums[mid]<=nums[nums.size()-1])
+            else if(nums[mid]<=nums[nums.size()-1]) //jodi right part sorted thake
             {
                 e=mid-1;
             }
         }
+      // ind store kora index jani mane holo:
+      // 0 index theke ind-1 akta sorted array
+      // ar ind theke n-1 porjonto akta sorted array
+      //akhon 2ta part e normal binary search call kore ans ber korbo
+      //binary search korte array, start, end, target lage..ar amader 4tai ache
         
        int x = BS(nums, 0, ind-1, target);
        int y = BS(nums, ind, nums.size()-1, target);
         
        if(x==-1 and y==-1)return -1;
-        else if(x==-1)return y;
-        else return x;
+       else if(x==-1)return y;
+       else return x;
         
     }
 };
