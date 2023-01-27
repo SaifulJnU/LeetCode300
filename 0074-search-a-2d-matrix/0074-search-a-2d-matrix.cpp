ed5038868ad7bb829fa25b556f628a04af840739
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-01-28 00:36:53 +0600

    Time: 4 ms (71.92%), Space: 9.6 MB (7.03%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+        //concept aditya verma
         int i=0;
         int j=matrix[0].size()-1;
         
-        int n=matrix.size();
-        int m=matrix[0].size();
+        int n=matrix.size();  //row size
+        int m=matrix[0].size(); //column size
         
-        while((i>=0 and i<n) and (j>=0 and j<m))
+        while((i>=0 and i<n) and (j>=0 and j<m)) //martix boundary condition
         {
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]>target) j--;
-            else if(matrix[i][j]<target) i++;
+            if(matrix[i][j]==target) return true; 
+            else if(matrix[i][j]>target) j--;    //go in the left side
+            else if(matrix[i][j]<target) i++;    //go in down side
         }
         
         return false;
