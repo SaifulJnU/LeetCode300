1983490474147a534ad6211e6abc0836ac2b68eb
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-01-28 03:32:06 +0600

    Time: 3 ms (70.13%), Space: 6.1 MB (7.05%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int mySqrt(int x) {
         
-        if(x==0)return 0;
+        if(x==0)return 0;//corner case
         int s=1;
         int e=x;
-        int mid;
         while(e-s>1)
         {
-            long long int mid=s+(e-s)/2;
-           
+            long long int mid=s+(e-s)/2; //long long because of int overflow issue
+            
             if(mid*mid==(long long)x)return mid;
             else if(mid*mid>(long long)x)
             {
@@ -18,7 +17,7 @@ public:
             else s=mid;
         }
         
-        return s; //here mid or s or e will return the same ans
+        return s; //here s or e will return the same ans
         
     }
 };
