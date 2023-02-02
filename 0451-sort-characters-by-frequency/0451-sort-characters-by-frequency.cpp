0615bef8d5e04e997e2dc8d97d24bd0a9f30e24b
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-03 05:35:08 +0600

    Time: 12 ms (78.79%), Space: 8.1 MB (89.69%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     
-      bool static cmp(pair<int,int>a,pair<int,int>b){
+    bool static cmp(pair<int,int>a,pair<int,int>b){
 
-    if(a.second==b.second){
-
+       if(a.second==b.second)
+       {
         return a.first<b.first;
+       }
+       return a.second>b.second;
 
     }
-
-    return a.second>b.second;
-
-}
     string frequencySort(string s) {
         
-      
-
-
-
-	     map<int,int>cnt;
+	   unordered_map<int,int>cnt;
         for(int x: s)
         {
             cnt[x]++;
@@ -28,12 +22,12 @@ public:
         for(auto i=cnt.begin();i!=cnt.end(); i++)
         {
              vp.push_back({i->first,i->second});
-     
         }
         
         sort(vp.begin(), vp.end(), cmp);
       
-        string vec;
+        s.clear();
+        
         for(auto i: vp)
         {
             int j=i.second;
@@ -41,13 +35,9 @@ public:
             while(j--)
             {
                
-               vec.push_back(i.first);
+               s.push_back(i.first);
             }
         }
-       // for(int i=0;i < vec.size(); i++)
-       // cout<<vec[i];
-       // cout<<endl;
-     
-        return vec;
+        return s;
     }
 };
