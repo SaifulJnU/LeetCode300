eb0dc559ee1053e6b34bb3909003678b9009aa78
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-02 06:50:46 +0600

    Time: 4 ms (95.99%), Space: 7.4 MB (61.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
         
-        if(s==t)return true;
-        else return false;
+//         O(nlogn) solution
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+        
+//         if(s==t)return true;
+//         else return false;
+        
+        
+        //O(n) solution
+        if(s.size()!=t.size())return false;
+        
+        int cnt[256]={0};
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            cnt[s[i]]++;
+            cnt[t[i]]--;
+        }
+        
+        for(int x: cnt)
+        {
+            if(x!=0)return false;
+        }
+        return true;
+        
     }
 };
