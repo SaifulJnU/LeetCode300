df8ca93c2ba4007069e33a880dde380ef411a5af
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-02 07:05:14 +0600

    Time: 25 ms (76.80%), Space: 13.9 MB (87.16%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,7 +4,7 @@ public:
         
         for(int i=num.size()-1; i>=0; i--)
         {
-            if(num[i]%2==1)
+            if(num[i] == '1' or num[i] == '3' or num[i] ==  '5' or num[i] == '7' or num[i] == '9')
             {
                 return num;
             }
