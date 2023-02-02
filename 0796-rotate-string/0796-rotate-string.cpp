6e597aba50b9d9345f661f703127b0557a59fa9d
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-02 06:15:08 +0600

    Time: 0 ms (100.00%), Space: 6 MB (87.74%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        
+        
+        if(s.size()!=goal.size())return false;
+        string add = s+s;
+        if(add.find(goal)!=string::npos)return true;
+        else return false;
+        
+    }
+};
