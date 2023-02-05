5583ddd78f822a3db767fc6b2f9bdd14774cd18c
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-06 02:51:09 +0600

    Time: 7 ms (38.81%), Space: 7.5 MB (29.10%) - LeetHub

---
+++ 1111-maximum-nesting-depth-of-two-valid-parentheses-strings/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxDepthAfterSplit(string seq) {
+        
+        vector<int>v;
+        int open=-1;
+        
+        for(int i=0; i<seq.size(); i++)
+        {
+            if(seq[i]=='(')
+            {
+                open++;
+                v.push_back(open%2);
+               
+            }
+            else
+            { 
+                v.push_back(open%2);
+                open--;
+               
+            }
+        }
+        
+        return v;
+    }
+};
