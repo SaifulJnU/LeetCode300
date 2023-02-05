ab38c0ace76dd499a309b96b53885a697654001d
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-06 00:34:28 +0600

    Time: 21 ms (13.65%), Space: 7.1 MB (18.27%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+        map<char, char> mp1;
+        map<char, char> mp2;
+        
+        
+        for(int i=0; i< s.size(); i++)
+        {
+            if((mp1.count(s[i])==1 and mp1[s[i]]!=t[i]) or (mp2.count(t[i])==1 and mp2[t[i]]!=s[i]))
+            {
+                return false;
+            }
+            
+            mp1.insert({s[i],t[i]});
+            mp2.insert({t[i],s[i]});
+
+        }
+        
+        return true;
+    }
+};
