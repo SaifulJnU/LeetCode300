85e09a06c56027c22972ac4a353f120f0c2ed3c9
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-13 08:57:16 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (92.11%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-       
-        return n/5+n/25+n/125+n/625+n/3125;
+   // zero can be formed by combination of (5,2) count the number of 5 in given n! basically do n/5
+   //      will give the number of 5 but there is a catch 25 has (5,5) that means we have to calculate
+   //      it one more time for that just divide the n in multiple of 5(5,25,125,625,3125) 
+   //      for 2,3,4 digits number
+      //  return n/5+n/25+n/125+n/625+n/3125;
+        
+        
+        int cnt=0;
+        
+        for(int i=5; i<=n; i+=5)
+        {
+            int x=i;
+            
+            while(x%5==0)
+            {
+                x/=5;
+                cnt++;
+
+            }
+        }
+        
+        return cnt;
+         
     }
 };
