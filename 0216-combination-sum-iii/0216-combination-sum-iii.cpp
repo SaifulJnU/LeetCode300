b29b875cee806b7a70ac94fa8bb478cf7dfe3ddc
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-14 05:28:00 +0600

    Time: 7 ms (14.49%), Space: 13.7 MB (6.06%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void solve(int k, int n, vector<vector<int>>&ans, vector<int>ds, int idx)
+    {
+        if(k==0)
+        {
+            if(n==0)
+            {
+                ans.push_back(ds);
+                //return;
+            }
+            
+            return ;
+        }
+         if(idx>9)return;
+        
+        //take
+        ds.push_back(idx);
+        solve(k-1,n-idx,ans, ds, idx+1);
+        ds.pop_back();
+        //not take
+        solve(k,n,ans, ds, idx+1);
+        
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        
+        vector<int>ds;
+        vector<vector<int>>ans;
+        
+        solve(k, n, ans, ds, 1);
+        
+        return ans;
+    }
+};
