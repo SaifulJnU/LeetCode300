94508b6afa0e158bec819c8776e2946d487d55bb
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-13 08:58:37 +0600

    Time: 2 ms (51.94%), Space: 6 MB (11.21%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -5,24 +5,26 @@ public:
    //      will give the number of 5 but there is a catch 25 has (5,5) that means we have to calculate
    //      it one more time for that just divide the n in multiple of 5(5,25,125,625,3125) 
    //      for 2,3,4 digits number
-      //  return n/5+n/25+n/125+n/625+n/3125;
+        
+       return n/5+n/25+n/125+n/625+n/3125;  //O(1) solution
         
         
-        int cnt=0;
+// O(n^2) solution       
+//         int cnt=0;
         
-        for(int i=5; i<=n; i+=5)
-        {
-            int x=i;
+//         for(int i=5; i<=n; i+=5)
+//         {
+//             int x=i;
             
-            while(x%5==0)
-            {
-                x/=5;
-                cnt++;
+//             while(x%5==0)
+//             {
+//                 x/=5;
+//                 cnt++;
 
-            }
-        }
+//             }
+//         }
         
-        return cnt;
+//         return cnt;
          
     }
 };
