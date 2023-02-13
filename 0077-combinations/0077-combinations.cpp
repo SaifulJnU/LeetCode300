3cf16f7b6cb007ff70cd7caf06db33cdfd9d1150
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-14 02:51:09 +0600

    Time: 29 ms (65.60%), Space: 18 MB (34.12%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -3,28 +3,27 @@ public:
     
     void solve(int idx, int n, int k, vector<vector<int>>&ans, vector<int>&ds)
     {
-        if(idx > n)   //as here idx is not index, it is direct value from 1 to n so > sign is used
-        {
-            if(k==0)
+         if(k==0)
             {
                 ans.push_back(ds);
                 return;
             }
+        if(idx > n)   //as here idx is not index, it is direct value from 1 to n so > sign is used
+        {
+           
             return;
         }
         
         //take
         ds.push_back(idx);
-        solve(idx+1, n, k-1, ans, ds);
+        solve(idx+1, n, k-1, ans, ds); //jehetu element ta nicchi tai idx+1 and k-1
         ds.pop_back();
         
         //not take
-        solve(idx+1, n, k, ans, ds);
+        solve(idx+1, n, k, ans, ds); //jehetu skip kortechi tai idx+1 and k
     }
     
     vector<vector<int>> combine(int n, int k) {
-        
-        
         vector<vector<int>>ans;
         vector<int>ds;
         
