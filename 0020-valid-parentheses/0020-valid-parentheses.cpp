00f8274192a97a62082f4f24ffad82f254a81008
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-19 02:42:19 +0600

    Time: 4 ms (39.23%), Space: 6.4 MB (55.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+    
+        stack<int>st;
+        for(int i =0; i<s.size(); i++)
+        {
+            if(s[i]=='(' or s[i]=='{' or s[i]=='[')
+            {
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty())
+                {
+                    return false;
+                }
+                else{
+                    if((s[i]== ')' and st.top() == '(' ) or (s[i] == '}' and st.top() == '{' ) or (s[i] == ']' and st.top() == '[' ))
+                    {
+                      st.pop();
+                    }
+                    else return false;
+                }
+            }
+
+        
+        }
+        return st.empty();
+        
+    }
+};
