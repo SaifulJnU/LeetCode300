215c631b41d0a40868a713ccad852e5a937eea08
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-20 09:16:15 +0600

    Time: 64 ms (5.14%), Space: 8.4 MB (95.62%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        for(int i=0; i<nums1.size(); i++)
+        {
+            int f=0;
+            
+            for(int j=0; j<nums2.size(); j++)
+            {
+                if(nums1[i]==nums2[j])
+                {
+                    for(int k=j+1; k<nums2.size(); k++)
+                    {
+                        if(nums1[i]<nums2[k])
+                        {
+                            f=1;
+                            ans.push_back(nums2[k]);
+                            break;
+                        }
+                    }
+                }
+            }
+            if(f==0)ans.push_back(-1);
+            
+        }
+        
+        return ans;
+    }
+};
