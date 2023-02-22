98cde572d65de296b62301773987fce8e5dc36de
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-22 07:00:16 +0600

    Time: 3 ms (96.16%), Space: 8.8 MB (66.81%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+       
+         stack<int>st;
+         unordered_map<int, int> umap;
+        
+         for(int i = nums2.size()-1; i>=0; i--)
+         {
+             if(st.empty())
+             {
+                 umap.insert({nums2[i],-1});
+             }
+             else if(!st.empty() and st.top()>nums2[i])
+             {
+                 umap.insert({nums2[i], st.top()});
+             }
+             else if(!st.empty() and st.top()<=nums2[i])
+             {
+                 while(!st.empty() and st.top()<=nums2[i])
+                 {
+                     st.pop();
+                 }
+                 
+                 if(st.empty())
+                 {
+                     umap.insert({nums2[i],-1});
+                 }
+                 else
+                 {
+                     umap.insert({nums2[i], st.top()});
+                 }
+             }
+            st.push(nums2[i]);
+         }
+        
         vector<int>ans;
-        for(int i=0; i<nums1.size(); i++)
+        for(auto x : nums1)
         {
-            int f=0;
-            
-            for(int j=0; j<nums2.size(); j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    for(int k=j+1; k<nums2.size(); k++)
-                    {
-                        if(nums1[i]<nums2[k])
-                        {
-                            f=1;
-                            ans.push_back(nums2[k]);
-                            break;
-                        }
-                    }
-                }
-            }
-            if(f==0)ans.push_back(-1);
-            
+            ans.push_back(umap[x]);  
         }
         
         return ans;
