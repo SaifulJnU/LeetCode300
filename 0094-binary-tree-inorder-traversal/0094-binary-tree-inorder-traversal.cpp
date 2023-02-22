1430b08cb482e97df6649681105d94f81428620c
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-22 13:00:22 +0600

    Time: 0 ms (100.00%), Space: 8.5 MB (46.52%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,16 +11,23 @@
  */
 class Solution {
 public:  
-    vector<int>v;
-    vector<int> inorderTraversal(TreeNode* root) {
-        
+   
+    void inOrder(TreeNode* root, vector<int>&v)
+    {
         if(root==NULL)
-            return {} ;
+            return;
         
-        inorderTraversal(root->left);
+        inOrder(root->left, v);
         v.push_back(root->val);
-        inorderTraversal(root->right);
+        inOrder(root->right, v);
         
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>v;
+        
+        inOrder(root, v);
+    
         return v;
     }
 };
