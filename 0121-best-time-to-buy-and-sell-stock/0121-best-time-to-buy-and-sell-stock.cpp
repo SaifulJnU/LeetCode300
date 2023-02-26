2d0f004b5212eb95be75085f77f9c770f0a474fe
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-02-26 13:34:38 +0600

    Time: 157 ms (26.39%), Space: 93.2 MB (91.87%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int maxProf=0;
+        int low=0;
+      
+        for(int i=0; i<prices.size(); i++)
+        {
+            if(prices[i]<prices[low])
+            {
+                low = i;
+                continue;
+            }
+
+            maxProf = max(maxProf, prices[i]-prices[low]);
+            
+        }
+        return maxProf;
+    }
+};
