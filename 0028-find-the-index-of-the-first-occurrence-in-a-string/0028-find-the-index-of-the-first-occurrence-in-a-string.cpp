4e58932a2227a00d1c93092a5964d2b7aa0c6bc6
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-04-03 02:22:21 +0600

    Time: 0 ms (100.00%), Space: 6.3 MB (71.16%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         
-        return haystack.find(needle);
+        //corner cases;
+        int m=needle.size();
+        int n=haystack.size();
+        
+        if(m==0)return 0;
+        if(m>n)return -1;
+        
+        for(int i=0; i<=n-m; i++)
+        {bool found = true;
+            for(int j=0; j<m; j++)
+            {
+                if(haystack[i+j]!=needle[j])
+                {
+                    found = false;
+                    break;
+                }
+            }
+           if(found)
+           {
+               return i;
+           }
+        }    
+        return -1;
     }
 };
