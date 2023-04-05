e8d3d8dd63f1ceb45afa6c337edeb1456152b56f
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2023-04-06 03:22:26 +0600

    Time: 50 ms (20.42%), Space: 18.4 MB (21.24%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,46 +1,63 @@
 class Solution {
-    
 private:
-    vector<int> x = {1,-1, 0, 0 };
-    vector<int> y = {0, 0, 1, -1};
+    vector<int> x = {+1, -1, 0, 0};
+    vector<int> y = {0, 0, +1, -1};
     
-    void dfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& vis) {
-        
-    if (row >= 0 && row < grid.size() && col >= 0 && col < grid[0].size() && !vis[row][col] && grid[row][col] == '1')
-    {
-        vis[row][col] = 1;
-        
-        for (int i = 0; i < 4; i++) {
-            int newRow = x[i] + row;
-            int newCol = y[i] + col;
-            dfs(newRow, newCol, grid, vis);
+    void dfs(int i, int j, vector<vector<bool>> &visited, vector<vector<char>>& grid){
+        if(i>=0 && i<grid.size() && j>=0 && j<grid[0].size() && !visited[i][j] && grid[i][j] == '1'){
+            visited[i][j] = true;
+            
+            for(int k=0; k<4; k++){
+                int newX = i+x[k];
+                int newY = j+y[k];
+                
+                dfs(newX, newY, visited, grid);
+            }
         }
     }
-}
     
-public:
+    bool isValid(int i, int j, vector<vector<bool>> &visited, vector<vector<char>>& grid){
+        if(i>=0 && i<grid.size() && j>=0 && j<grid[0].size() && !visited[i][j] && grid[i][j] == '1') return true;
+        else
+            return false;
+    }
     
-    int numIslands(vector<vector<char>>& grid) {
+    void bfs(int i, int j, vector<vector<bool>> &visited, vector<vector<char>>& grid){
+        queue<pair<int, int>> q1;
+        q1.push({i, j});
+        visited[i][j] = true;
         
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        //vis matrix
-        vector<vector<int>>vis(n, vector<int>(m,0));
-        
-        int cnt=0;
-        
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<m; j++)
-            {
-                if(!vis[i][j] and grid[i][j] == '1')
-                {
-                    cnt++;
-                    dfs(i, j, grid, vis);
+        while(!q1.empty()){
+            pair<int, int> front = q1.front();
+            q1.pop();
+            
+            for(int k=0; k<4; k++){
+                int newX = front.first+x[k];
+                int newY = front.second+y[k];
+                
+                if(isValid(newX, newY, visited, grid)){
+                    q1.push({newX, newY});
+                    visited[newX][newY] = true;
                 }
             }
         }
-        return cnt;
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size(), n=grid[0].size();
+        
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        int count = 0;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    count++;
+                    bfs(i, j, visited, grid);
+                }
+            }
+        }
+        
+        return count;
     }
 };
